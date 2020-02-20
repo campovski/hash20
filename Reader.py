98da0fce5962cbde767cb0e48045585f1276dde6
easybi-mc <marcel.campa@easy.bi>
2020-02-20 19:04:31 +0100

    Add reader

---
+++ Reader.py
@@ -0,0 +1,20 @@
+class Reader:
+    def __init__(self, filename):
+        self.libraries = []
+        with open(filename, 'r') as f:
+            first_line = f.readline().split()
+            self.B = int(first_line[0])
+            self.L = int(first_line[1])
+            self.D = int(first_line[3])
+            second_line = f.readline().split()
+            self.scores = map(lambda x: int(x), second_line)
+            for _ in range(self.B):
+                library = {}
+                tmp = f.readline().split()
+                first_line_l = map(lambda x: int(x), tmp)
+                library['signup'] = first_line_l[1]
+                library['canShip'] = first_line_l[2]
+                tmp = f.readline().split()
+                library['books'] = sorted(map(lambda x: int(x), tmp))
+                self.libraries.append(library)
+
